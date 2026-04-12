432226e7eef40f7937af8b9d1a33ba7b4841e271
prabhjotkaur26 <prabh008968@gmail.com>
2026-04-12 17:27:05 +0530

    Update app.py

--- lambdas/vote/vote.zip/app.py
+++ lambdas/vote/vote.zip/app.py
@@ -1,1 +1,42 @@
 
+import boto3
+import os
+import json
+from datetime import datetime
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(os.environ['TABLE_NAME'])
+
+def lambda_handler(event, context):
+
+    body = json.loads(event['body'])
+
+    voter_id = body["voterId"]
+    election_id = body["electionId"]
+    post = body["post"]
+    candidate_id = body["candidateId"]
+
+    pk = f"VOTER#{voter_id}"
+    sk = f"{election_id}#{post}"
+
+    try:
+        table.put_item(
+            Item={
+                "PK": pk,
+                "SK": sk,
+                "candidateId": candidate_id,
+                "time": str(datetime.utcnow())
+            },
+            ConditionExpression="attribute_not_exists(PK)"
+        )
+
+        return {
+            "statusCode": 200,
+            "body": json.dumps({"message": "Vote success"})
+        }
+
+    except Exception:
+        return {
+            "statusCode": 400,
+            "body": json.dumps({"message": "Already voted"})
+        }
