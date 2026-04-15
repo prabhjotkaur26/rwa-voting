ae06a55428a0631a24aa76bba3f5d3db011c3a1b
prabhjotkaur26 <prabh008968@gmail.com>
2026-04-15 22:30:05 +0530

    Update iam.tf

--- infra/iam.tf
+++ infra/iam.tf
@@ -1,31 +1,30 @@
-resource "aws_iam_role" "lambda_role" {
-  name = "lambda-exec-role"
+resource "aws_iam_role_policy" "lambda_policy" {
+  name = "lambda-policy"
+  role = aws_iam_role.lambda_role.id
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Action = "sts:AssumeRole",
-      Effect = "Allow",
-      Principal = {
-        Service = "lambda.amazonaws.com"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+
+      # DynamoDB access
+      {
+        Effect = "Allow"
+        Action = [
+          "dynamodb:Scan",
+          "dynamodb:GetItem"
+        ]
+        Resource = "*"
+      },
+
+      # S3 access
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:PutObject",
+          "s3:GetObject"
+        ]
+        Resource = "*"
       }
-    }]
+    ]
   })
-}
-
-resource "aws_iam_policy_attachment" "lambda_basic" {
-  name       = "lambda-basic"
-  roles      = [aws_iam_role.lambda_role.name]
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
-
-resource "aws_iam_role_policy_attachment" "ses_access" {
-  role       = aws_iam_role.lambda_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSESFullAccess"
-}
-
-resource "aws_iam_policy_attachment" "dynamo_access" {
-  name       = "dynamo-access"
-  roles      = [aws_iam_role.lambda_role.name]
-  policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
 }
