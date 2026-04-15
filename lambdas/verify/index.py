14bce20effe2f72164672c8bc7b9f66148f7469f
prabhjotkaur26 <prabh008968@gmail.com>
2026-04-15 22:44:13 +0530

    Update index.py

--- lambdas/verify/index.py
+++ lambdas/verify/index.py
@@ -1,9 +1,38 @@
-import jwt
+import json, boto3, os, jwt, time
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(os.environ['OTP_TABLE'])
 
 SECRET = os.environ['JWT_SECRET']
 
-token = jwt.encode(
-    {"email": email, "exp": int(time.time()) + 3600},
-    SECRET,
-    algorithm="HS256"
-)
+def lambda_handler(event, context):
+
+    body = event.get('body')
+    if isinstance(body, str):
+        body = json.loads(body)
+
+    email = body['email']
+    otp = body['otp']
+
+    res = table.get_item(Key={'email': email})
+
+    if 'Item' not in res:
+        return {"statusCode": 400, "body": "Invalid"}
+
+    item = res['Item']
+
+    if item['used'] or item['otp'] != otp or item['expiry'] < int(time.time()):
+        return {"statusCode": 400, "body": "Invalid OTP"}
+
+    table.update_item(
+        Key={'email': email},
+        UpdateExpression="SET used = :u",
+        ExpressionAttributeValues={":u": True}
+    )
+
+    token = jwt.encode({"email": email}, SECRET, algorithm="HS256")
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({"token": token})
+    }
