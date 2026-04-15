55b5c60030b14acd4329a7594ed812cdb891a6a0
prabhjotkaur26 <prabh008968@gmail.com>
2026-04-15 22:46:06 +0530

    Update index.py

--- lambdas/export/index.py
+++ lambdas/export/index.py
@@ -1,15 +1,28 @@
-import boto3, csv, os
+import json, boto3, os, csv
 
+dynamodb = boto3.resource('dynamodb')
 s3 = boto3.client('s3')
+
+table = dynamodb.Table(os.environ['VOTE_TABLE'])
+BUCKET = os.environ['BUCKET']
 
 def lambda_handler(event, context):
 
-    file = "/tmp/results.csv"
+    res = table.scan()
+    items = res.get("Items", [])
 
-    with open(file, "w") as f:
+    file_path = "/tmp/results.csv"
+
+    with open(file_path, "w", newline="") as f:
         writer = csv.writer(f)
-        writer.writerow(["Post", "Candidate", "Votes"])
+        writer.writerow(["Email", "Post", "Candidate"])
 
-    s3.upload_file(file, os.environ['BUCKET'], "results.csv")
+        for i in items:
+            writer.writerow([i['email'], i['post_id'], i['candidate_id']])
 
-    return {"statusCode": 200, "body": "Export ready"}
+    s3.upload_file(file_path, BUCKET, "results.csv")
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({"message": "Exported to S3"})
+    }
