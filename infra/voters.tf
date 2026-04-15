bfc6d3d7c9c6028ea976a8d0ebba2da07bbcbe50
prabhjotkaur26 <prabh008968@gmail.com>
2026-04-15 23:02:35 +0530

    Update voters.tf

--- infra/voters.tf
+++ infra/voters.tf
@@ -1,14 +1,12 @@
 resource "aws_dynamodb_table_item" "voters" {
-  for_each = toset([
-    "prabh008968@gmail.com",
-    "kaurprabhsidhu852004@gmail.com",
-    "prabhjot582004@gmail.com"
-  ])
+  for_each = toset(var.voter_emails)
 
   table_name = aws_dynamodb_table.voters.name
   hash_key   = "email"
 
   item = jsonencode({
-    email = each.value
+    email = {
+      S = each.value
+    }
   })
 }
