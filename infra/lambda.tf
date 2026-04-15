a15efed158078ac5d3336bf8fa0019521add8667
prabhjotkaur26 <prabh008968@gmail.com>
2026-04-15 22:49:32 +0530

    Update lambda.tf

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -1,7 +1,104 @@
+# AUTH
+data "archive_file" "auth_zip" {
+  type        = "zip"
+  source_dir  = "../lambdas/auth"
+  output_path = "auth.zip"
+}
+
+resource "aws_lambda_function" "auth" {
+  function_name = "auth-function"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "index.lambda_handler"
+  runtime       = "python3.11"
+
+  filename         = data.archive_file.auth_zip.output_path
+  source_code_hash = data.archive_file.auth_zip.output_base64sha256
+
+  environment {
+    variables = {
+      OTP_TABLE    = aws_dynamodb_table.otp.name
+      VOTER_TABLE  = aws_dynamodb_table.voters.name
+      SENDER_EMAIL = "your-verified-email@gmail.com"
+    }
+  }
+}
+
+# VERIFY
+data "archive_file" "verify_zip" {
+  type        = "zip"
+  source_dir  = "../lambdas/verify"
+  output_path = "verify.zip"
+}
+
+resource "aws_lambda_function" "verify" {
+  function_name = "verify-function"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "index.lambda_handler"
+  runtime       = "python3.11"
+
+  filename         = data.archive_file.verify_zip.output_path
+  source_code_hash = data.archive_file.verify_zip.output_base64sha256
+
+  environment {
+    variables = {
+      OTP_TABLE  = aws_dynamodb_table.otp.name
+      JWT_SECRET = "mysecret123"
+    }
+  }
+}
+
+# VOTE
+data "archive_file" "vote_zip" {
+  type        = "zip"
+  source_dir  = "../lambdas/vote"
+  output_path = "vote.zip"
+}
+
+resource "aws_lambda_function" "vote" {
+  function_name = "vote-function"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "index.lambda_handler"
+  runtime       = "python3.11"
+
+  filename         = data.archive_file.vote_zip.output_path
+  source_code_hash = data.archive_file.vote_zip.output_base64sha256
+
+  environment {
+    variables = {
+      VOTE_TABLE = aws_dynamodb_table.votes.name
+      JWT_SECRET = "mysecret123"
+    }
+  }
+}
+
+# ADMIN
+data "archive_file" "admin_zip" {
+  type        = "zip"
+  source_dir  = "../lambdas/admin"
+  output_path = "admin.zip"
+}
+
+resource "aws_lambda_function" "admin" {
+  function_name = "admin-function"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "index.lambda_handler"
+  runtime       = "python3.11"
+
+  filename         = data.archive_file.admin_zip.output_path
+  source_code_hash = data.archive_file.admin_zip.output_base64sha256
+
+  environment {
+    variables = {
+      VOTE_TABLE = aws_dynamodb_table.votes.name
+    }
+  }
+}
+
+# EXPORT
 data "archive_file" "export_zip" {
   type        = "zip"
   source_dir  = "../lambdas/export"
-  output_path = "${path.module}/export.zip"
+  output_path = "export.zip"
 }
 
 resource "aws_lambda_function" "export" {
@@ -13,15 +110,10 @@ resource "aws_lambda_function" "export" {
   filename         = data.archive_file.export_zip.output_path
   source_code_hash = data.archive_file.export_zip.output_base64sha256
 
-  timeout      = 10
-  memory_size  = 256
-
   environment {
     variables = {
       VOTE_TABLE = aws_dynamodb_table.votes.name
       BUCKET     = aws_s3_bucket.candidate_images.bucket
     }
   }
-
-  depends_on = [data.archive_file.export_zip]
 }
