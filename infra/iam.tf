953c15f5d491d2a6d25f0ee0c2a6a969056fab41
prabhjotkaur26 <prabh008968@gmail.com>
2026-04-15 18:55:54 +0530

    Create iam.tf

---
+++ infra/iam.tf
@@ -0,0 +1,32 @@
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda-exec-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_policy_attachment" "lambda_basic" {
+  name       = "lambda-basic"
+  roles      = [aws_iam_role.lambda_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_policy_attachment" "sns_access" {
+  name       = "sns-access"
+  roles      = [aws_iam_role.lambda_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSNSFullAccess"
+}
+
+resource "aws_iam_policy_attachment" "dynamo_access" {
+  name       = "dynamo-access"
+  roles      = [aws_iam_role.lambda_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
+}
