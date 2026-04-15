2379fa6fb634d6c9952b46308ab3d76c00cb6716
prabhjotkaur26 <prabh008968@gmail.com>
2026-04-15 22:44:51 +0530

    Update index.py

--- lambdas/vote/index.py
+++ lambdas/vote/index.py
@@ -1,30 +1,41 @@
-import jwt, os
+import json, boto3, os, jwt
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(os.environ['VOTE_TABLE'])
 
 SECRET = os.environ['JWT_SECRET']
 
 def lambda_handler(event, context):
 
-    headers = event['headers']
+    headers = event.get("headers", {})
     token = headers.get("Authorization")
 
     try:
-        decoded = jwt.decode(token, SECRET, algorithms=["HS256"])
-        email = decoded['email']
+        data = jwt.decode(token, SECRET, algorithms=["HS256"])
+        email = data['email']
     except:
         return {"statusCode": 401, "body": "Unauthorized"}
 
-    body = json.loads(event['body'])
+    body = event.get('body')
+    if isinstance(body, str):
+        body = json.loads(body)
 
     post_id = body['post_id']
     candidate_id = body['candidate_id']
 
-    table.put_item(
-        Item={
-            "post_id": post_id,
-            "voter_id": email,
-            "candidate_id": candidate_id
-        },
-        ConditionExpression="attribute_not_exists(voter_id)"
-    )
+    try:
+        table.put_item(
+            Item={
+                "email": email,
+                "post_id": post_id,
+                "candidate_id": candidate_id
+            },
+            ConditionExpression="attribute_not_exists(email)"
+        )
+    except:
+        return {"statusCode": 400, "body": "Already voted"}
 
-    return {"statusCode": 200, "body": "Vote recorded"}
+    return {
+        "statusCode": 200,
+        "body": "Vote recorded"
+    }
