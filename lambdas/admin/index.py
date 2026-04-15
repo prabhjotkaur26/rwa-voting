531c5ae6dc82dc068a127353131043a26f9ebcc4
prabhjotkaur26 <prabh008968@gmail.com>
2026-04-15 22:45:44 +0530

    Update index.py

--- lambdas/admin/index.py
+++ lambdas/admin/index.py
@@ -1,1 +1,25 @@
+import json, boto3, os
 
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(os.environ['VOTE_TABLE'])
+
+def lambda_handler(event, context):
+
+    res = table.scan()
+    items = res.get("Items", [])
+
+    result = {}
+
+    for i in items:
+        post = i['post_id']
+        cand = i['candidate_id']
+
+        if post not in result:
+            result[post] = {}
+
+        result[post][cand] = result[post].get(cand, 0) + 1
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps(result)
+    }
