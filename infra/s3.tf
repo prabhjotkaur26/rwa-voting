ffa96ccf64851172c08b9145c2b3b7260f3c643a
prabhjotkaur26 <prabh008968@gmail.com>
2026-04-16 21:52:14 +0530

    Update s3.tf

--- infra/s3.tf
+++ infra/s3.tf
@@ -1,9 +1,33 @@
 resource "aws_s3_bucket" "candidate_images" {
-  bucket = "${var.project_name}-images"
-
+  bucket        = "${var.project_name}-images"
   force_destroy = true
+
+  tags = {
+    Name        = "${var.project_name}-images"
+    Environment = "dev"
+  }
 }
 
+# ✅ BLOCK PUBLIC ACCESS (VERY IMPORTANT)
+resource "aws_s3_bucket_public_access_block" "block" {
+  bucket = aws_s3_bucket.candidate_images.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# ✅ ENABLE VERSIONING (BEST PRACTICE)
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.candidate_images.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# ✅ ENABLE ENCRYPTION
 resource "aws_s3_bucket_server_side_encryption_configuration" "enc" {
   bucket = aws_s3_bucket.candidate_images.id
 
