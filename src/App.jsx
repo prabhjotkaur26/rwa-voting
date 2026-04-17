087ec9189697244c89b4142294d6862c578f2c3f
prabhjotkaur26 <prabh008968@gmail.com>
2026-04-17 21:58:32 +0530

    Refactor App component to simplify routing
    Removed ProtectedRoute component and updated routes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,23 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
+import VerifyOtp from "./pages/VerifyOtp";
 import Vote from "./pages/Vote";
-import Results from "./pages/Results";
 import VotingDashboard from "./pages/VotingDashboard";
 import Layout from "./components/Layout";
 
-// 🔐 Protected Route Component
-function ProtectedRoute({ children }) {
-  const token = localStorage.getItem("token");
-
-  if (!token) {
-    return <Navigate to="/" replace />;
-  }
-
-  return children;
-}
-
-export default function App() {
+function App() {
   return (
     <BrowserRouter>
       <Layout>
         <Routes>
-
-          {/* 🔓 Public Route */}
           <Route path="/" element={<Login />} />
-
-          {/* 🔐 Protected Routes */}
-          <Route
-            path="/vote"
-            element={
-              <ProtectedRoute>
-                <Vote />
-              </ProtectedRoute>
-            }
-          />
-
-          <Route
-            path="/results"
-            element={
-              <ProtectedRoute>
-                <Results />
-              </ProtectedRoute>
-            }
-          />
-
-          <Route
-            path="/dashboard"
-            element={
-              <ProtectedRoute>
-                <VotingDashboard />
-              </ProtectedRoute>
-            }
-          />
-
+          <Route path="/verify" element={<VerifyOtp />} />
+          <Route path="/vote" element={<Vote />} />
+          <Route path="/admin" element={<VotingDashboard />} />
         </Routes>
       </Layout>
     </BrowserRouter>
   );
 }
+
+export default App;
