9872d87c3f513b232c1e24a5e965882dc6c5fcb2
prabhjotkaur26 <prabh008968@gmail.com>
2026-04-18 00:46:23 +0530

    Update layout.jsx

--- frontend/src/components/layout.jsx
+++ frontend/src/components/layout.jsx
@@ -1,1 +1,12 @@
+export default function Layout({ children }) {
+  return (
+    <div>
+      <nav className="bg-blue-600 text-white p-4">
+        RWA Voting System
+      </nav>
+
+      <main>{children}</main>
+    </div>
+  );
+}
 
