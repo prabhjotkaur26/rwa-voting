1df0e7bbac82979f3f25e9ce9e3019ffda3807c0
prabhjotkaur26 <prabh008968@gmail.com>
2026-04-17 22:21:24 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
-import VerifyOtp from "./pages/VerifyOtp";
 import Vote from "./pages/Vote";
-import VotingDashboard from "./pages/VotingDashboard";
-import Layout from "./components/Layout";
 
-function App() {
+export default function App() {
   return (
     <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/verify" element={<VerifyOtp />} />
-          <Route path="/vote" element={<Vote />} />
-          <Route path="/admin" element={<VotingDashboard />} />
-        </Routes>
-      </Layout>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/vote" element={<Vote />} />
+      </Routes>
     </BrowserRouter>
   );
 }
-
-export default App;
