56a8f83c4bdf34ae0f3ae8831527ccbd4f8e8cec
prabhjotkaur26 <prabh008968@gmail.com>
2026-04-18 02:38:11 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { sendOtp } from "../api/api";
+import { sendOtp } from "../api/api.js";
 
 export default function Login() {
   const [email, setEmail] = useState("");
