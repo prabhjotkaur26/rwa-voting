417957b3ca6ef251102a06f569f6c1b51ac8b896
prabhjotkaur26 <prabh008968@gmail.com>
2026-04-18 23:34:49 +0530

    Update outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,16 +1,31 @@
+########################################
+# API GATEWAY URL
+########################################
 output "api_gateway_url" {
   description = "Base URL of HTTP API Gateway"
   value       = aws_apigatewayv2_api.api.api_endpoint
 }
 
+########################################
+# FRONTEND WEBSITE URL
+########################################
 output "frontend_url" {
-  value = aws_s3_bucket_website_configuration.frontend.website_endpoint
+  description = "S3 hosted frontend URL"
+  value       = aws_s3_bucket_website_configuration.frontend.website_endpoint
 }
 
+########################################
+# FRONTEND BUCKET NAME
+########################################
 output "frontend_bucket_name" {
-  value = aws_s3_bucket.frontend.id
+  description = "Frontend S3 bucket name"
+  value       = aws_s3_bucket.frontend.id
 }
 
-output "images_bucket_name" {
-  value = aws_s3_bucket.candidate_images.id
+########################################
+# CSV UPLOAD BUCKET NAME
+########################################
+output "csv_bucket_name" {
+  description = "CSV upload bucket name"
+  value       = aws_s3_bucket.csv_bucket.id
 }
