14edf0d1cd4311cfff841c11ea6e10f864b1c11a
prabhjotkaur26 <prabh008968@gmail.com>
2026-04-18 17:04:20 +0530

    Add AWS Lambda permission for S3 invocation

--- infra/main.tf
+++ infra/main.tf
@@ -145,6 +145,11 @@ resource "aws_s3_bucket_notification" "bucket_notify" {
     lambda_function_arn = aws_lambda_function.csv_lambda.arn
     events              = ["s3:ObjectCreated:*"]
   }
-
-  depends_on = [aws_lambda_permission.allow_s3]
+}
+resource "aws_lambda_permission" "allow_s3" {
+  statement_id  = "AllowS3Invoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.csv_lambda.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.csv_bucket.arn
 }
