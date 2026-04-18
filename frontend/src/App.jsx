6acc09498f9d3c991549fdbc6f09351e1aaad7e7
prabhjotkaur26 <prabh008968@gmail.com>
2026-04-18 19:51:37 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,7 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
-import OTPVerify from "./pages/OTPVerify";
-import Dashboard from "./pages/Dashboard";
-import Admin from "./pages/Admin";
-import Results from "./pages/Results";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/verify" element={<OTPVerify />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/admin" element={<Admin />} />
-        <Route path="/results" element={<Results />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <Login />;
 }
 
 export default App;
