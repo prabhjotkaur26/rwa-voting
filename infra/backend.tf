52af80b3468aaf2cd6022b75eef1ea266297933e
prabhjotkaur26 <prabh008968@gmail.com>
2026-04-20 02:45:05 +0530

    Update backend.tf

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "rwa-tf-state-bucket"
+    bucket         = "rwa-tf-state-backend-bucket"
     key            = "global/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "rwa-tf-lock"
