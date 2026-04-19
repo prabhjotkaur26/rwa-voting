43ede93075dd8e42242a498664fb230b3f80c1b0
prabhjotkaur26 <prabh008968@gmail.com>
2026-04-19 22:47:16 +0530

    Update lambda.tf

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -275,27 +275,3 @@ resource "aws_lambda_function" "results" {
     }
   }
 }
-########################################
-# LAMBDA FUNCTION
-########################################
-resource "aws_lambda_function" "csv_lambda" {
-  function_name = "csv_to_dynamodb"
-  role          = aws_iam_role.lambda_role.arn
-  handler       = "index.lambda_handler"
-  runtime       = "python3.11"
-
-  filename = "${path.root}/infra/build/csv_lambda.zip"
-
-  timeout     = 300
-  memory_size = 512
-
-  environment {
-    variables = {
-      VOTER_TABLE = "voter-registry"
-    }
-  }
-
-  depends_on = [
-    aws_iam_role_policy_attachment.lambda_basic
-  ]
-}
