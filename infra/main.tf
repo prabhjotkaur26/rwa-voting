be025fc8452292e1fa1368d1cfcf9cebd3545f65
prabhjotkaur26 <prabh008968@gmail.com>
2026-04-20 00:14:00 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -39,7 +39,7 @@ resource "aws_lambda_function" "csv_lambda" {
   handler       = "index.lambda_handler"
   runtime       = "python3.11"
 
-  filename = "${path.module}/lambdas/csv_lambda/csv_lambda.zip"
+ filename = "${path.module}/build/csv_lambda.zip"
 
   timeout     = 300
   memory_size = 512
