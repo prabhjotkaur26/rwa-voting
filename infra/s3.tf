ad65eb6924c9be42c0293218158b99cb01542173
prabhjotkaur26 <prabh008968@gmail.com>
2026-04-20 02:17:29 +0530

    Update s3.tf

--- infra/s3.tf
+++ infra/s3.tf
@@ -73,14 +73,3 @@ resource "aws_s3_bucket_policy" "frontend_policy" {
     ]
   })
 }
-resource "aws_s3_bucket_website_configuration" "frontend" {
-  bucket = aws_s3_bucket.frontend.id
-
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "index.html"
-  }
-}
