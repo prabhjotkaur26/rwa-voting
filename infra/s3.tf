6a1b90080e4a3ada421273f4ffb6cbc541a78ef4
prabhjotkaur26 <prabh008968@gmail.com>
2026-04-20 02:09:03 +0530

    Update s3.tf

--- infra/s3.tf
+++ infra/s3.tf
@@ -25,13 +25,13 @@ resource "aws_s3_bucket_ownership_controls" "ownership" {
 ########################################
 # BLOCK PUBLIC ACCESS
 ########################################
-resource "aws_s3_bucket_public_access_block" "block" {
-  bucket = aws_s3_bucket.candidate_images.id
+resource "aws_s3_bucket_public_access_block" "frontend_block" {
+  bucket = aws_s3_bucket.frontend.id
 
-  block_public_acls      = true
-  block_public_policy    = true
-  ignore_public_acls     = true
-  restrict_public_buckets = true
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
 ########################################
