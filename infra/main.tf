0155f2cf6ffddf08a7a43adf5d94417f6bda2922
prabhjotkaur26 <prabh008968@gmail.com>
2026-04-19 21:01:40 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -47,7 +47,7 @@ data "archive_file" "csv_zip" {
 ########################################
 resource "aws_lambda_function" "csv_lambda" {
   function_name = "csv_to_dynamodb"
-  role          = aws_iam_role.lambda_role.arn   # ✅ from iam.tf
+  role          = aws_iam_role.lambda_role.arn
   handler       = "index.lambda_handler"
   runtime       = "python3.11"
 
@@ -57,9 +57,17 @@ resource "aws_lambda_function" "csv_lambda" {
   timeout     = 300
   memory_size = 512
 
+  # 🔥 ADD THIS BLOCK
+  environment {
+    variables = {
+      VOTER_TABLE = "voter-registry"
+    }
+  }
+
   depends_on = [
-    aws_iam_role_policy_attachment.lambda_basic   # ✅ from iam.tf
+    aws_iam_role_policy_attachment.lambda_basic
   ]
+}
 
   tags = {
     Name = "csv-lambda"
