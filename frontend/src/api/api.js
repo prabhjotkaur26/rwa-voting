4440e148f5b739260c0bcafae601179b209ea0f7
prabhjotkaur26 <prabh008968@gmail.com>
2026-04-20 13:17:11 +0530

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,12 +1,12 @@
 import { API } from "./config/api.js";
 
-export const sendOtp = (email) =>
-  fetch(`${API}/send-otp`, {
+export const sendOtp = async (email) => {
+  return await fetch(`${API}/send-otp`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ email })
   });
-
+};
 export const verifyOtp = (email, otp) =>
   fetch(`${API}/verify-otp`, {
     method: "POST",
