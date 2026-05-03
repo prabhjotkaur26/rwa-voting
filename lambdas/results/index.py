6468ed2017eb252c0feb1aed7b1e4cd25a83881b
prabhjotkaur26 <prabh008968@gmail.com>
2026-05-03 09:40:16 +0530

    Update index.py

--- lambdas/results/index.py
+++ lambdas/results/index.py
@@ -1,10 +1,27 @@
 import json
 import boto3
 import os
-from boto3.dynamodb.conditions import Key
 
 dynamodb = boto3.resource("dynamodb")
 votes_table = dynamodb.Table(os.environ["VOTES_TABLE"])
+
+
+def lambda_handler(event, context):
+    try:
+        data = votes_table.scan()
+
+        result = {}
+
+        for item in data.get("Items", []):
+            candidate = item.get("candidateId")
+            if candidate:
+                result[candidate] = result.get(candidate, 0) + 1
+
+        return response(200, result)
+
+    except Exception as e:
+        print(e)
+        return response(500, "Error")
 
 
 def response(status, body):
@@ -16,59 +33,3 @@ def response(status, body):
         },
         "body": json.dumps(body)
     }
-
-
-def lambda_handler(event, context):
-    try:
-        print("EVENT:", json.dumps(event))
-
-        request_context = event.get("requestContext", {})
-        http = request_context.get("http", {})
-        method = http.get("method", "")
-        path = http.get("path", "")
-
-        # -----------------------------
-        # GET ALL RESULTS
-        # -----------------------------
-        if path == "/results" and method == "GET":
-            data = votes_table.scan()
-
-            result = {}
-
-            for item in data.get("Items", []):
-                candidate = item.get("candidateId")
-                if candidate:
-                    result[candidate] = result.get(candidate, 0) + 1
-
-            return response(200, result)
-
-        # -----------------------------
-        # GET RESULTS BY electionId + postId
-        # -----------------------------
-        elif path.startswith("/results/") and method == "GET":
-            parts = path.strip("/").split("/")
-
-            # expected: /results/{electionId}/{postId}
-            if len(parts) == 3:
-                _, electionId, postId = parts
-
-                data = votes_table.query(
-                    KeyConditionExpression=Key("PK").eq(f"{electionId}#{postId}")
-                )
-
-                result = {}
-
-                for item in data.get("Items", []):
-                    candidate = item.get("candidateId")
-                    if candidate:
-                        result[candidate] = result.get(candidate, 0) + 1
-
-                return response(200, result)
-
-            return response(400, {"message": "Invalid path format"})
-
-        return response(400, {"message": "Invalid request"})
-
-    except Exception as e:
-        print("ERROR:", str(e))
-        return response(500, {"message": "Internal server error"})
