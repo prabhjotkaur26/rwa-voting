1df1ba8635a36aa101e9c87a0a996ed117c46ec5
prabhjotkaur26 <prabh008968@gmail.com>
2026-05-08 22:33:17 +0530

    Update auth.js

--- frontend/static/js/auth.js
+++ frontend/static/js/auth.js
@@ -2,9 +2,43 @@ async function sendOTP() {
 
   const email = document.getElementById("email").value;
 
-  localStorage.setItem("email", email);
+  if (!email) {
+    alert("Please enter email");
+    return;
+  }
 
-  alert("OTP Sent Successfully");
+  try {
 
-  window.location.href = "verify.html";
+    const response = await fetch(
+      "https://7p57z2eau2.execute-api.ap-south-1.amazonaws.com/send-otp",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ email })
+      }
+    );
+
+    const data = await response.json();
+
+    if (response.ok) {
+
+      localStorage.setItem("email", email);
+
+      alert(data.message);
+
+      window.location.href = "/frontend/templates/verify.html";
+
+    } else {
+
+      alert(data.message);
+    }
+
+  } catch (error) {
+
+    console.error(error);
+
+    alert("Something went wrong");
+  }
 }
