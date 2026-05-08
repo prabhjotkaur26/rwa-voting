b98a145506118eb30b364315ee51912767680943
prabhjotkaur26 <prabh008968@gmail.com>
2026-05-08 17:21:06 +0530

    Create config.js

---
+++ frontend/static/js/config.js
@@ -0,0 +1,3 @@
+// Update this value after deploying your API Gateway.
+// It should match the HTTP API endpoint exported by Terraform.
+const API_BASE = "https://7p57z2eau2.execute-api.ap-south-1.amazonaws.com";
