ec9c7721e039e4ec16fbe822aa28c9a0e41a01cc
prabhjotkaur26 <prabh008968@gmail.com>
2026-05-08 17:21:51 +0530

    Delete frontend/config.js

--- frontend/config.js
+++
@@ -1,3 +0,0 @@
-// Update this value after deploying your API Gateway.
-// It should match the HTTP API endpoint exported by Terraform.
-const API_BASE = "https://7p57z2eau2.execute-api.ap-south-1.amazonaws.com";
