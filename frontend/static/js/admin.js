0c03687eeb09ea58c3cf11db633e248fccce9fa6
prabhjotkaur26 <prabh008968@gmail.com>
2026-05-15 23:41:58 +0530

    Update admin.js

--- frontend/static/js/admin.js
+++ frontend/static/js/admin.js
@@ -1,14 +1,37 @@
-function adminLogin() {
+async function loadDashboard() {
 
-  const user = document.getElementById("adminUser").value;
-  const pass = document.getElementById("adminPass").value;
+  try {
 
-  if (user === "admin" && pass === "admin123") {
+    const response = await fetch(
+      "https://7p57z2eau2.execute-api.ap-south-1.amazonaws.com/results"
+    );
 
-    window.location.href = "dashboard.html";
+    const data = await response.json();
 
-  } else {
+    console.log(data);
 
-    alert("Invalid Credentials");
+    let totalVotes = 0;
+
+    Object.values(data).forEach(votes => {
+      totalVotes += votes;
+    });
+
+    document.getElementById("totalVotes").innerText = totalVotes;
+
+    const totalVoters = 150;
+
+    document.getElementById("totalVoters").innerText = totalVoters;
+
+    const participation =
+      ((totalVotes / totalVoters) * 100).toFixed(1);
+
+    document.getElementById("participation").innerText =
+      participation + "%";
+
+    updateChart(data);
+
+  } catch (error) {
+
+    console.error(error);
   }
 }
